28b682b31141725c86c823f155b74630242d1b7b
Tony Pham <topham@adobe.com>
2020-10-05 19:39:42 -0700

    abstract spoitfy api call into spotify.js

--- src/App.js
+++ src/App.js
@@ -1,73 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Login from './components/Login';
-import { getTokenFromUrl } from './spotify';
-import SpotifyWebApi from "spotify-web-api-js";
-import Player from './components/Player'
-import { useStateProviderValue } from './StateProvider'
-
-//Create new instance of SpotifyWebApi to interact with Spotify API
-const spotify = new SpotifyWebApi();
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import { getTokenFromUrl } from "./spotify";
+import Player from "./components/Player";
+import { useStateProviderValue } from "./StateProvider";
+import { spotify } from "./spotify";
 
 function App() {
   //dispatch is a function that allows us to interact with the StateContext layer. We can dispatch actions to update state
-  const [{ user, token }, dispatch] = useStateProviderValue();
+  const [{ user, playlist, token }, dispatch] = useStateProviderValue();
 
-  useEffect( () => {
+  useEffect(() => {
     //When we are redirected back to the app compoenent, the app component is rerendered with the new url. Grab access token from url at that point
     const hash = getTokenFromUrl();
-    window.location.hash=""; //clear the access token within the url
+    window.location.hash = ""; //clear the access token within the url
     //define _token with underscore to differentiate between variable local to the function and token state variable
     const _token = hash.access_token;
     // console.log(hash)
-  
+
     //sanity check
     if (_token) {
       // setToken(_token);
       //insted of setting token in state, dispatch it to StateProvider
       dispatch({
-        type: 'SET_TOKEN',
-        token: _token
-      })
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
       //Pass access token to SpotifyWebApi (community developed wrapper for spotify api)
       spotify.setAccessToken(_token);
       //getMe() will return a promise
-      spotify.getMe().then(user => {
+      spotify.getMe().then((user) => {
         //dispatch user data to StateProvider instead of setting state. Ensure correct object is passed as defined in the reducer file
         dispatch({
-          type: 'SET_USER',
-          user: user
+          type: "SET_USER",
+          user: user,
           //user (shorthand for user: user in ES6)
-        })        
-      })
+        });
+      });
       //Dispatch user's playlist to StatePovider layer
-      spotify.getUserPlaylists().then(playlists => {
+      spotify.getUserPlaylists().then((playlists) => {
         dispatch({
-          type: 'SET_PLAYLISTS',
-          playlists: playlists
-        })
-      })
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
 
-      spotify.getPlaylist('37i9dQZEVXcTYvdwTEpTSD').then(playlist => {
+      spotify.getPlaylist("37i9dQZEVXcTYvdwTEpTSD").then((playlist) => {
         dispatch({
-          type: 'SET_DISCOVER_WEEKLY',
-          discover_weekly: playlist
-        })
-      })
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: playlist,
+        });
+      });
     }
-
   }, []);
 
   return (
     <div className="app">
-      {
-        token ? (
-          <Player spotify={spotify}/>
-          ) : (
-            <Login />
-          )
-      }
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -1,37 +1,42 @@
 //Where all the spotify logic will live
 //only use capitalize naming convention for react components
+import SpotifyWebApi from "spotify-web-api-js";
 
-export const AUTH_END_POINT = 'https://accounts.spotify.com/authorize';
+export const AUTH_END_POINT = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000/";
 const clientId = "5d156ee5b4c74b0494f9040df2e5c5e5";
+
+export const spotify = new SpotifyWebApi();
 
 //identify scopes to be requested from the Spotify documentation
 // https://developer.spotify.com/documentation/general/guides/scopes/#user-read-recently-played
 const scopes = [
-    "user-read-currently-playing",
-    "user-read-recently-played",
-    "user-read-playback-state",
-    "user-top-read",
-    "user-modify-playback-state"
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
 ];
 
 //Provided access token prepended to redirect url. Obtain token by parsing the url string on the current window
 export const getTokenFromUrl = () => {
-    //http://localhost:3000/#access_token=BQAziY4R6aOl9wCsNRb-Lgae2bHMPGpJCAcWEq5X
-    //Find #, take the substring starting at 1, stop at next & (which is the next parameter)
-    return window.location.hash
-        .substring(1)
-        .split('&')
-        .reduce((initial, item) => {
-            //split between the access_token=BQA....
-            let parts = item.split('=');
-            //decode uri compoenent, the key
-            initial[parts[0]] = decodeURIComponent(parts[1]);
-            
-            return initial;
-        }, {});
-}
+  //http://localhost:3000/#access_token=BQAziY4R6aOl9wCsNRb-Lgae2bHMPGpJCAcWEq5X
+  //Find #, take the substring starting at 1, stop at next & (which is the next parameter)
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      //split between the access_token=BQA....
+      let parts = item.split("=");
+      //decode uri compoenent, the key
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
 
 //create login url
 //response_type = token
-export const loginUrl = `${AUTH_END_POINT}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+export const loginUrl = `${AUTH_END_POINT}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
